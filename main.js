8d40d1c7518842b77e15c931089c04aebeefef5f
Ernest Hubbard <ernhub89@gmail.com>
2019-10-01 09:11:57 -0700

    matched 2 cards

--- main.js
+++ main.js
@@ -1,10 +1,48 @@
 $(document).ready(initializeApp);
 
 function initializeApp() {
-  $('.cardfront').on('click', handleCardClick);
-
+  $('.card').on('click', handleCardClick);
+  $('#modalYouWon').hide();
+}
+function showCard() {
+  $('.cardfront').removeClass('hidden')
 }
 function handleCardClick(event) {
-  $('.cardfront').addClass('hidden')
-  $('.cardback').show();
-}
+  console.log(event);
+  var parent = $(event.currentTarget);
+  var child = parent.find('.cardfront');
+  child.addClass('hidden');
+  if (firstCardClicked === null) {
+    firstCardClicked = $(event.currentTarget);
+    firstImage = firstCardClicked.find('.cardback');
+    imageSource = firstImage.css('background-image');
+  }
+  else if (firstCardClicked !== null) {
+    secondCardClicked = $(event.currentTarget);
+    secondImage = secondCardClicked.find('.cardback');
+    secondImageSource = secondImage.css('background-image');
+    if (imageSource === secondImageSource) {
+      console.log('match');
+      matches = matches + 1;
+      firstCardClicked = null;
+      if (matches === maxMatches) {
+        $("#modalYouWon").modal({
+          escapeClose: false,
+          clickClose: false,
+          showClose: false
+        });
+      }
+    } else if (imageSource !== secondImageSource) {
+      setTimeout(showCard, 1500);
+      firstCardClicked = null;
+    }
+  }
+};
+var firstImage;
+var imageSource;
+var secondImage;
+var secondImageSource;
+var firstCardClicked = null;
+var secondCardClicked = null;
+var matches = null;
+var maxMatches = 1;
