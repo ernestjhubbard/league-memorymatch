0e9038301eb72752d8585b9998b2011e23219bcd
Ernest Hubbard <ernhub89@gmail.com>
2019-10-06 07:34:07 -0700

    commit before adding sounds

--- main.js
+++ main.js
@@ -1,25 +1,95 @@
 $(document).ready(initializeApp);
-//Global variables
 var gamesPlayed = ('.gamesplayedli');
-var attempts = null;
+var attempts = 0;
 var games_played = null;
-var matches_max = 2;
-var matches = null;
+var matches_max = 14;
+var matches = 0;
 var firstCardClicked = null;
 var secondCardClicked = null;
 var cardDiv1;
 var cardDiv2;
+//--------------------------GLOBAL VARIABLES END--------------------------
+
+//--------------------------INITIALIZE APP BEGIN--------------------------
 function initializeApp() {
-  $('.card').on('click', handleCardClick);
-  $('#modalYouWon').hide();
+  var difficulty;
+  $("#myModal").modal({
+    escapeClose: false,
+    clickClose: false,
+    showClose: false
+  });
+  $('.resetcurrentgame').on('click', function () {
+    switch (difficulty) {
+      case 'illaoi':
+        shuffleAndAppend('illaoi');
+        break;
+      case 'swain':
+        shuffleAndAppend('swain');
+        break;
+      case 'darius':
+        shuffleAndAppend('darius');
+        break;
+    }
+    ++games_played;
+    resetStats();
+  });
+  $('.resetfullgame').on('click', function () {
+    resetStats();
+    $('.carddiv').remove();
+    $("#myModal").modal({
+      escapeClose: false,
+      clickClose: false,
+      showClose: false
+    });
+  });
+  $('.uldifficulty').on('mouseenter', function () {
+    $('.difficulty').removeClass('hidden');
+  })
+  $('.uldifficulty').on('mouseleave', function () {
+    $('.difficulty').addClass('hidden');
+  })
+  $('.ulreset').on('mouseenter', function () {
+    $('.reset').removeClass('hidden');
+  })
+  $('.ulreset').on('mouseleave', function () {
+    $('.reset').addClass('hidden');
+  })
+  $('.introbutton1').on('click', function () {
+    difficulty = 'illaoi'; //sets correct amount for reset button
+    $('.video2').addClass('hidden');
+    $('.video3').addClass('hidden');
+    $('.video1').removeClass('hidden');
+    $('.jquery-modal').hide();
+    setTimeout(shuffleAndAppend('illaoi'), 500);
+  })
+  $('.introbutton2').on('click', function () {
+    difficulty = 'swain';
+    $('.video1').addClass('hidden');
+    $('.video3').addClass('hidden');
+    $('.video2').removeClass('hidden');
+    $('.jquery-modal').hide();
+    setTimeout(shuffleAndAppend('swain'), 500);
+  })
+  $('.introbutton3').on('click', function () {
+    difficulty = 'darius';
+    $('.video1').addClass('hidden');
+    $('.video2').addClass('hidden');
+    $('.video3').removeClass('hidden');
+    $('.jquery-modal').hide();
+    setTimeout(shuffleAndAppend('darius'), 500);
+  })
 }
+//--------------------------INITIALIZE APP END--------------------------
 function showCard() {
   firstCardClicked = null;
   secondCardClicked = null;
 }
+
+//--------------------------CARD CLICK BEGIN--------------------------
 function handleCardClick(event) {
+  console.log(event.currentTarget)
   var found = $(event.currentTarget).find('.disableclick').hasClass('disableclick');
-  if(found === true){
+  if (found === true) {
     return;
   }
   if (firstCardClicked !== null && secondCardClicked !== null) {
@@ -28,14 +98,15 @@ function handleCardClick(event) {
   if (firstCardClicked === null && secondCardClicked === null) {
     cardDiv1 = $(event.currentTarget).find('.cardfront');
     firstCardClicked = $(event.currentTarget).find('.cardback');
+    firstCardSibling = $(event.currentTarget).find('.cardfront');
     firstImage = firstCardClicked.css('background-image');
     firstCardClicked.removeClass('hidden');
     attempts++;
   }
   else if (firstCardClicked !== null) {
-    displayStats()
     cardDiv2 = $(event.currentTarget).find('.cardfront');
     secondCardClicked = $(event.currentTarget).find('.cardback')
+    secondCardSibling = $(event.currentTarget).find('.cardfront')
     secondImage = secondCardClicked.css('background-image');
     secondCardClicked.removeClass('hidden');
     if (secondCardClicked.is(firstCardClicked)) {
@@ -44,37 +115,140 @@ function handleCardClick(event) {
     }
     if (firstImage === secondImage) {
       console.log('match');
-      matches++;
+      ++matches;
+      secondCardClicked.addClass('disableclick');
+      firstCardClicked.addClass('disableclick');
+      displayStats();
       setTimeout(function () {
-        secondCardClicked.addClass('disableclick');
-        firstCardClicked.addClass('disableclick');
+        firstCardClicked.addClass('hidden');
+        firstCardSibling.addClass('hidden');
+        secondCardClicked.addClass('hidden');
+        secondCardSibling.addClass('hidden');
         firstCardClicked = null;
         secondCardClicked = null;
       }, 1500);
       if (matches === matches_max) {
-        $("#modalYouWon").modal({
+        $(".modal").modal({
           escapeClose: false,
           clickClose: false,
           showClose: false
         });
+        resetStats();
       }
-    } setTimeout(function () {
-      firstCardClicked.addClass('hidden');
-      secondCardClicked.addClass('hidden');
-      firstCardClicked = null;
-      secondCardClicked = null;
-
-    }, 1500);
+    } else {
+      setTimeout(function () {
+        firstCardClicked.addClass('hidden');
+        secondCardClicked.addClass('hidden');
+        firstCardClicked = null;
+        secondCardClicked = null;
+      }, 1500);
+    }
   }
 };
-function calculateAccuracy() {
-  var accuracy = matches / attempts;
-  return accuracy;
-}
+//--------------------------CARD CLICK END--------------------------
+
+//--------------------------STATS FUNCTIONS BEGIN--------------------------
 function displayStats() {
-  var newAccuracy = calculateAccuracy()
-  newAccuracy *= 100;
+  function calculateAccuracy() {
+    return matches / attempts;
+  }
+  var newAccuracy = calculateAccuracy();
+  if (isNaN(newAccuracy)) {
+    newAccuracy = 0;
+  }
+  var percent = newAccuracy * 100;
   var accuracyClass = $('.accuracyli')
-  accuracyClass.text(" " + newAccuracy.toFixed(0) + ' %');
-  $('.attemptsli').text(attempts.toString());
+  accuracyClass.text(" " + percent.toFixed(0) + ' %');
+  $('.attemptsli').text(attempts);
+  $('.matchesli').text(matches);
+  $('.gamesplayedli').text(games_played);
 }
+function resetStats() {
+  $('.attemptsli').text('');
+  $('.accuracyli').text('');
+  $('.matches').text('');
+  attempts = 0;
+  matches = 0;
+  $('.gamesplayedli').text(games_played);
+  var returnCards = $('.cardback');
+  returnCards.removeClass('disableclick');
+}
+
+//--------------------------STATS FUNCTIONS END--------------------------
+
+//--------------------------DYNAMIC AND SHUFFLE BEGIN--------------------------
+function shuffleAndAppend(difficulty) {
+  $('.carddiv').remove();
+  var classArray = [
+    'illaoi',
+    'illaoi',
+    'darius',
+    'darius',
+    'riven',
+    'riven',
+    'veigar',
+    'veigar',
+    'ziggs',
+    'ziggs',
+    'mordekaiser',
+    'mordekaiser',
+    'vi',
+    'vi',
+    'kayn',
+    'kayn',
+    'shyvana',
+    'shyvana',
+    'teemo',
+    'teemo',
+    'caitlyn',
+    'caitlyn',
+    'lux',
+    'lux',
+    'masteryi',
+    'masteryi',
+    'vayne',
+    'vayne',
+    'annie',
+    'annie',
+    'twitch',
+    'twitch'
+  ];
+  var newLength;
+  switch (difficulty) {
+    case 'illaoi':
+      $('li').css('color', 'rgb(221, 243, 124)');
+      matches_max = 12;
+      newLength = classArray.length - 8;
+      classArray.length = newLength;
+      while (classArray.length) {
+        var randomChamp = Math.floor(Math.random() * classArray.length);
+        var classChosen = classArray.splice(randomChamp, 1);
+        $('.main').append($(`<div class="carddiv"><div class="card" onclick="handleCardClick(event)"><div class="cardfront cardfrontillaoi"></div><div class="${classChosen} cardback hidden">`));
+      }
+      $('.carddiv').css({ 'width': '16%', 'height': '29%' });
+      break;
+    case 'swain':
+      $('li').css('color', 'rgb(243, 105, 105)');
+      matches_max = 14;
+      newLength = classArray.length - 4;
+      classArray.length = newLength;
+      while (classArray.length) {
+        var randomChamp = Math.floor(Math.random() * classArray.length);
+        var classChosen = classArray.splice(randomChamp, 1);
+        $('.main').append($(`<div class="carddiv"><div class="card" onclick="handleCardClick(event)"><div class="cardfront cardfrontswain"></div><div class="${classChosen} cardback hidden">`));
+      }
+      $('.carddiv').css({ 'width': '14%', 'height': '25%' })
+      break;
+    case 'darius':
+      $('li').css('color', '#ccc3c3');
+      matches_max = 16;
+      while (classArray.length) {
+        var randomChamp = Math.floor(Math.random() * classArray.length);
+        var classChosen = classArray.splice(randomChamp, 1);
+        $('.main').append($(`<div class="carddiv"><div class="card" onclick="handleCardClick(event)"><div class="cardfront cardfrontdarius"></div><div class="${classChosen} cardback hidden">`));
+      }
+      $('.carddiv').css({ 'height': '26%' })
+      break;
+  }
+}
+//--------------------------DYNAMIC AND SHUFFLE END--------------------------
